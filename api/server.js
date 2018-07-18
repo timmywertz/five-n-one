78221933b21ee5537b702b14b604184bc6d7bae0
Timmy Wertz <timmylwertz@gmail.com>
2018-07-18 16:13:15 -0400

    initial commit

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors({ credentials: true }));
+
+app.get("/", (req, res) => res.send("5n1 API Server"));
+
+app.listen(5000);
+console.log("Server listening at 5000");
---
+++ app/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "tachyons";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
